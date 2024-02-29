cb32dbbba33893ec9c87141ba9fec668f011e3aa
Helias Karagozidis <karagozidis@gmail.com>
2024-02-29 14:58:56 +0200

    Blockchain Implementations

--- src/main/java/com/eurodyn/repository/blockchain_api/BlockchainRepository.java
+++ src/main/java/com/eurodyn/repository/blockchain_api/BlockchainRepository.java
@@ -1,7 +1,27 @@
 package com.eurodyn.repository.blockchain_api;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Repository
-public class BlockchainRepository {
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+@Service
+public class BlockChainRepository {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public Boolean pnrAlreadyExists(String id) {
+
+        Query query = this.entityManager.createNativeQuery(
+                "SELECT id, created_by FROM pnr_request_outgoing WHERE id = :id ");
+        query.setParameter("id", id);
+
+        List<Object[]> rows = query.getResultList();
+
+        return !rows.isEmpty();
+    }
+
 }
